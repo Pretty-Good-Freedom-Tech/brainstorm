8ea0e2950d7c11a2dcf7f93bc38b5bd99b9b2ea6
David Strayhorn <david.strayhorn4@gmail.com>
2025-03-16 17:10:50 -0400

    update profile page to address api call 502 errors

--- src/algos/exportWhitelist.sh
+++ src/algos/exportWhitelist.sh
@@ -2,7 +2,7 @@
 
 source /etc/hasenpfeffr.conf # NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD
 
-echo "Running exportWhiteList. This script updates /usr/local/lib/node_modules/hasenpfeffr/src/plugins/whitelist_pubkeys.json based on personalizedPageRank scores stored in neo4j."
+echo "Running exportWhiteList. This script updates /usr/local/lib/node_modules/hasenpfeffr/plugins/whitelist_pubkeys.json based on personalizedPageRank scores stored in neo4j."
 
 CYPHER1="
 MATCH (n:NostrUser) WHERE n.personalizedPageRank > 0.000001
@@ -39,7 +39,7 @@ done < /usr/local/lib/node_modules/hasenpfeffr/src/algos/whitelistQueryOutput.tx
 
 echo "}" >> /usr/local/lib/node_modules/hasenpfeffr/src/algos/whitelist_pubkeys.json
 
-sudo mv /usr/local/lib/node_modules/hasenpfeffr/src/algos/whitelist_pubkeys.json /usr/local/lib/node_modules/hasenpfeffr/src/plugins/whitelist_pubkeys.json
+sudo mv /usr/local/lib/node_modules/hasenpfeffr/src/algos/whitelist_pubkeys.json /usr/local/lib/node_modules/hasenpfeffr/plugins/whitelist_pubkeys.json
 
 # clean up
 sudo rm /usr/local/lib/node_modules/hasenpfeffr/src/algos/whitelistQueryOutput.txt
