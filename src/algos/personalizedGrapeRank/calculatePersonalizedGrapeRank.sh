306c1019461b23dff9a952d7b788d9aea0c2c530
David Strayhorn <david.strayhorn4@gmail.com>
2025-03-30 13:05:39 -0400

    fux bug in calculatePersonalizedGrapeRank

--- src/algos/personalizedGrapeRank/calculatePersonalizedGrapeRank.sh
+++ src/algos/personalizedGrapeRank/calculatePersonalizedGrapeRank.sh
@@ -7,9 +7,11 @@ source /etc/graperank.conf   # Rating and confidence values
 touch ${HASENPFEFFR_LOG_DIR}/calculatePersonalizedGrapeRank.log
 sudo chown hasenpfeffr:hasenpfeffr ${HASENPFEFFR_LOG_DIR}/calculatePersonalizedGrapeRank.log
 
+echo "$(date): Starting calculatePersonalizedGrapeRank"
 echo "$(date): Starting calculatePersonalizedGrapeRank" >> ${HASENPFEFFR_LOG_DIR}/calculatePersonalizedGrapeRank.log
 
-echo "HASENPFEFFR_OWNER_PUBKEY: $HASENPFEFFR_OWNER_PUBKEY"
+echo "$(date): Continuing calculatePersonalizedGrapeRank ... starting cypher queries"
+echo "$(date): Continuing calculatePersonalizedGrapeRank ... starting cypher queries" >> ${HASENPFEFFR_LOG_DIR}/calculatePersonalizedGrapeRank.log
 
 CYPHER0="
 MATCH (user:NostrUser)
@@ -50,27 +52,43 @@ cypher-shell -a "$NEO4J_URI" -u "$NEO4J_USER" -p "$NEO4J_PASSWORD" "$CYPHER1" > 
 cypher-shell -a "$NEO4J_URI" -u "$NEO4J_USER" -p "$NEO4J_PASSWORD" "$CYPHER2" > $TEMP_DIR/mutes.csv
 cypher-shell -a "$NEO4J_URI" -u "$NEO4J_USER" -p "$NEO4J_PASSWORD" "$CYPHER3" > $TEMP_DIR/reports.csv
 
+echo "$(date): Continuing calculatePersonalizedGrapeRank ... finished cypher queries, starting initializeRatings"
+echo "$(date): Continuing calculatePersonalizedGrapeRank ... finished cypher queries, starting initializeRatings" >> ${HASENPFEFFR_LOG_DIR}/calculatePersonalizedGrapeRank.log
+
 # create one large raw data object oRatingsReverse.json of format: [context][ratee][rater] = [score, confidence]
 sudo bash $THIS_DIR/initializeRatings.sh
+
+echo "$(date): Continuing calculatePersonalizedGrapeRank ... finished initializeRatings, calling initializeScorecards"
+echo "$(date): Continuing calculatePersonalizedGrapeRank ... finished initializeRatings, calling initializeScorecards" >> ${HASENPFEFFR_LOG_DIR}/calculatePersonalizedGrapeRank.log
 
 # intialize oScorecards: iterate through ratees.csv and create empty objects for each ratee
 sudo bash $THIS_DIR/initializeScorecards.sh
 
+echo "$(date): Continuing calculatePersonalizedGrapeRank ... finished initializeScorecards, calling calculateGrapeRank"
+echo "$(date): Continuing calculatePersonalizedGrapeRank ... finished initializeScorecards, calling calculateGrapeRank" >> ${HASENPFEFFR_LOG_DIR}/calculatePersonalizedGrapeRank.log
+
 # iterate through GrapeRank until max iterations or until convergence
 sudo bash $THIS_DIR/calculateGrapeRank.sh
 
+echo "$(date): Continuing calculatePersonalizedGrapeRank ... finished calculateGrapeRank, calling updateNeo4j"
+echo "$(date): Continuing calculatePersonalizedGrapeRank ... finished calculateGrapeRank, calling updateNeo4j" >> ${HASENPFEFFR_LOG_DIR}/calculatePersonalizedGrapeRank.log
+
 # update Neo4j with data from scorecards.json
 sudo bash $THIS_DIR/updateNeo4j.sh
+
+echo "$(date): Continuing calculatePersonalizedGrapeRank ... finished updateNeo4j, starting clean up"
+echo "$(date): Continuing calculatePersonalizedGrapeRank ... finished updateNeo4j, starting clean up" >> ${HASENPFEFFR_LOG_DIR}/calculatePersonalizedGrapeRank.log
 
 # clean up tmp files
 
 # Update the WHEN_LAST_CALCULATED timestamp in the configuration file
 TIMESTAMP=$(date +%s)
 TMP_CONF=$(mktemp)
-cat ${CONFIG_FILES[graperank]} | sed "s/^export WHEN_LAST_CALCULATED=.*$/export WHEN_LAST_CALCULATED=$TIMESTAMP/" > "$TMP_CONF"
-sudo cp "$TMP_CONF" ${CONFIG_FILES[graperank]}
-sudo chmod 644 ${CONFIG_FILES[graperank]}
-sudo chown root:hasenpfeffr ${CONFIG_FILES[graperank]}
+cat /etc/graperank.conf | sed "s/^export WHEN_LAST_CALCULATED=.*$/export WHEN_LAST_CALCULATED=$TIMESTAMP/" > "$TMP_CONF"
+sudo cp "$TMP_CONF" /etc/graperank.conf
+sudo chmod 644 /etc/graperank.conf
+sudo chown root:hasenpfeffr /etc/graperank.conf
 rm "$TMP_CONF"
 
+echo "$(date): Finished calculatePersonalizedGrapeRank"
 echo "$(date): Finished calculatePersonalizedGrapeRank" >> ${HASENPFEFFR_LOG_DIR}/calculatePersonalizedGrapeRank.log
