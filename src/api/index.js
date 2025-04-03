963a7f3be9b2feebbe8c6159fecc765e3dd5292b
David Strayhorn <david.strayhorn4@gmail.com>
2025-04-03 14:06:42 -0400

    start to use getConfigFromFile more consistently from utils/config module

--- src/api/index.js
+++ src/api/index.js
@@ -19,36 +19,8 @@ const {
     authMiddleware 
 } = require('./auth/authHandler');
 
-// For configuration
-const fs = require('fs');
-
-/**
- * Get configuration values directly from /etc/hasenpfeffr.conf
- * @param {string} varName - Name of the configuration variable
- * @param {*} defaultValue - Default value to return if variable is not found
- * @returns {string} Value of the configuration variable or default value
- */
-function getConfigFromFile(varName, defaultValue = null) {
-    try {
-        const confFile = '/etc/hasenpfeffr.conf';
-        if (fs.existsSync(confFile)) {
-            // Read the file content directly
-            const fileContent = fs.readFileSync(confFile, 'utf8');
-            
-            // Look for the variable in the file content
-            const regex = new RegExp(`${varName}=[\"\'](.*?)[\"\']`, 'gm');
-            const match = regex.exec(fileContent);
-            
-            if (match && match[1]) {
-                return match[1];
-            }
-        }
-    } catch (error) {
-        console.error(`Error reading config for ${varName}:`, error);
-    }
-    
-    return defaultValue;
-}
+// Import utilities
+const { getConfigFromFile } = require('../utils/config');
 
 /**
  * Register all API endpoints with the Express app
