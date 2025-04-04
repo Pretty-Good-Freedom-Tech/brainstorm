d8b6aa3a4cf0566c77af6a72dc0d13fbf248c143
David Strayhorn <david.strayhorn4@gmail.com>
2025-04-03 22:16:32 -0400

    add handlePublishKind10040 to export/nip85/index.js

--- src/api/export/nip85/index.js
+++ src/api/export/nip85/index.js
@@ -5,10 +5,12 @@
 
 const { handleGenerateNip85 } = require('./commands/generate');
 const { handleCreateKind10040 } = require('./commands/kind10040');
+const { handlePublishKind10040 } = require('./commands/kind10040');
 
 // Export handlers directly - this allows the central router 
 // to register endpoints without creating multiple routers
 module.exports = {
     handleGenerateNip85,
-    handleCreateKind10040
+    handleCreateKind10040,
+    handlePublishKind10040
 };
