8d8162ec5b3e7e1b3f2e44d89086127de73fe7f5
David Strayhorn <david.strayhorn4@gmail.com>
2025-04-03 22:20:17 -0400

    remove GET support for publish-kind10040 endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -97,8 +97,7 @@ function register(app) {
     app.post('/api/generate-nip85', nip85.handleGenerateNip85);
     app.post('/api/create-kind10040', nip85.handleCreateKind10040);
     app.post('/api/publish-kind10040', nip85.handlePublishKind10040);
-    app.get('/api/publish-kind10040', nip85.handlePublishKind10040); // Support GET for backward compatibility
-    
+
     console.log('Registered all Hasenpfeffr API endpoints');
 }
 
