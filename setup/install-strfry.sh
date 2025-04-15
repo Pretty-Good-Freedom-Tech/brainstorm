5cbfd4967d9a74b3848548b89ce674136f8d5037
David Strayhorn <david.strayhorn4@gmail.com>
2025-04-15 11:46:48 -0400

    update nginx to add relay to strfry endpoint

--- setup/install-strfry.sh
+++ setup/install-strfry.sh
@@ -208,8 +208,19 @@ server {
         include /etc/nginx/mime.types;
     }
     
-    # Strfry relay at /strfry path
+    # moving Hasenpfeffr Control Panel to /
     location / {
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header Host \$host;
+        proxy_pass http://127.0.0.1:7778/;
+        proxy_http_version 1.1;
+
+        # Add this to ensure proper MIME types for all static files
+        include /etc/nginx/mime.types;
+    }
+
+    # Strfry relay at /relay path
+    location /relay {
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header Host \$host;
         proxy_pass http://127.0.0.1:7777/;
