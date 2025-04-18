130e95a60dd2c071bef9809ffbdd7f7a1ad1acaa
David Strayhorn <david.strayhorn4@gmail.com>
2025-04-18 18:41:14 -0400

    fix bug in calculateVerifiedFollowers script; make its api endpoint require auth

--- src/algos/calculateVerifiedFollowers.sh
+++ src/algos/calculateVerifiedFollowers.sh
@@ -11,11 +11,12 @@ sudo chown brainstorm:brainstorm ${BRAINSTORM_LOG_DIR}/calculateVerifiedFollower
 echo "$(date): Starting calculateVerifiedFollowers"
 echo "$(date): Starting calculateVerifiedFollowers" >> ${BRAINSTORM_LOG_DIR}/calculateVerifiedFollowers.log
 
-CYPHER1=`MATCH (f:NostrUser)-[:FOLLOWS]->(u:NostrUser)
+CYPHER1="
+MATCH (f:NostrUser)-[:FOLLOWS]->(u:NostrUser)
 WHERE f.influence > 0.1
 WITH u, COUNT(f) AS verifiedFollowers
 SET u.verifiedFollowerCount = verifiedFollowers
-RETURN COUNT(u) AS usersUpdated, SUM(verifiedFollowers) AS totalVerifiedFollowersSet`
+RETURN COUNT(u) AS usersUpdated, SUM(verifiedFollowers) AS totalVerifiedFollowersSet"
 
 cypherResults=$(sudo cypher-shell -a "$NEO4J_URI" -u "$NEO4J_USER" -p "$NEO4J_PASSWORD" "$CYPHER1")
 numUsersUpdated="${cypherResults:11}"
