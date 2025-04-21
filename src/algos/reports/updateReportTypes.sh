19804592bd7403dff0c781bd4aa8403eeefd70a9
David Strayhorn <david.strayhorn4@gmail.com>
2025-04-21 18:47:27 -0400

    remove double quotes from array of report types

--- src/algos/reports/updateReportTypes.sh
+++ src/algos/reports/updateReportTypes.sh
@@ -18,13 +18,11 @@ RETURN reportType
 
 cypherResults1=$(sudo cypher-shell -a "$NEO4J_URI" -u "$NEO4J_USER" -p "$NEO4J_PASSWORD" "$CYPHER1")
 
-# update reportTypes.json
-
 # remove existing reportTypes.json
 sudo rm -f ${BRAINSTORM_MODULE_ALGOS_DIR}/reports/reportTypes.json
 
-# write new reportTypes.json
-sudo echo "$cypherResults1" > ${BRAINSTORM_MODULE_ALGOS_DIR}/reports/reportTypes.json   
+# write new reportTypes.json, removing quotes
+sudo echo "$cypherResults1" | sed 's/"//g' > ${BRAINSTORM_MODULE_ALGOS_DIR}/reports/reportTypes.json   
 
 echo "$(date): Finished updateReportTypes"
 echo "$(date): Finished updateReportTypes" >> ${BRAINSTORM_LOG_DIR}/updateReportTypes.log
