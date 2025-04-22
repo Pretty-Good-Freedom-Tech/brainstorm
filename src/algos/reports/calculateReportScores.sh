3ec30fa391fe787c63fa43a219dbd4895e49c0a6
David Strayhorn <david.strayhorn4@gmail.com>
2025-04-21 20:16:50 -0400

    edit calculateReportScores to init all scores to zero

--- src/algos/reports/calculateReportScores.sh
+++ src/algos/reports/calculateReportScores.sh
@@ -14,6 +14,33 @@ sudo $BRAINSTORM_MODULE_ALGOS_DIR/reports/updateReportTypes.sh
 # import array of report types
 REPORT_TYPES=$(cat ${BRAINSTORM_MODULE_ALGOS_DIR}/reports/reportTypes.json)
 
+# loop through report types; for each user, initialize report counts
+for reportType in ${REPORT_TYPES[@]}; do
+    cypherResults1=$(sudo cypher-shell -a "$NEO4J_URI" -u "$NEO4J_USER" -p "$NEO4J_PASSWORD" "
+MATCH (u:NostrUser)
+SET u.nip56_${reportType}_grapeRankScore = 0
+SET u.nip56_${reportType}_reportCount = 0
+SET u.nip56_${reportType}_verifiedReportCount = 0
+RETURN COUNT(u) AS numReportedUsers")
+    numReportedUsers="${cypherResults1:11}"
+    echo "$(date): for reportType: $reportType; numReportedUsers: $numReportedUsers"
+    echo "$(date): for reportType: $reportType; numReportedUsers: $numReportedUsers" >> ${BRAINSTORM_LOG_DIR}/calculateReportScores.log
+done
+
+cypherResults1=$(sudo cypher-shell -a "$NEO4J_URI" -u "$NEO4J_USER" -p "$NEO4J_PASSWORD" "
+MATCH (u:NostrUser)
+SET u.nip56_totalGrapeRankScore = 0
+SET u.nip56_totalReportCount = 0
+SET u.nip56_totalVerifiedReportCount = 0
+RETURN COUNT(u) AS numReportedUsers")
+numReportedUsers="${cypherResults1:11}"
+
+echo "$(date): for reportType: total; numReportedUsers: $numReportedUsers"
+echo "$(date): for reportType: total; numReportedUsers: $numReportedUsers" >> ${BRAINSTORM_LOG_DIR}/calculateReportScores.log
+
+echo "$(date): Finished calculateReportScores"
+echo "$(date): Finished calculateReportScores" >> ${BRAINSTORM_LOG_DIR}/calculateReportScores.log
+
 # loop through report types; for each reported user, count the total number as well as the influence-weighted number of reports of that type by verified users
 for reportType in ${REPORT_TYPES[@]}; do
     cypherResults1=$(sudo cypher-shell -a "$NEO4J_URI" -u "$NEO4J_USER" -p "$NEO4J_PASSWORD" "
@@ -48,17 +75,28 @@ for reportType in ${REPORT_TYPES[@]}; do
 done 
 
 # remove final " + " from the end of TOTAL_REPORT_COUNT, TOTAL_VERIFIED_REPORT_COUNT, and TOTAL_GRAPE_RANK_SCORE
-TOTAL_REPORT_COUNT=${TOTAL_REPORT_COUNT%+}
-TOTAL_VERIFIED_REPORT_COUNT=${TOTAL_VERIFIED_REPORT_COUNT%+}
-TOTAL_GRAPE_RANK_SCORE=${TOTAL_GRAPE_RANK_SCORE%+}
+TOTAL_REPORT_COUNT="${TOTAL_REPORT_COUNT::-3}"
+TOTAL_VERIFIED_REPORT_COUNT="${TOTAL_VERIFIED_REPORT_COUNT::-3}"
+TOTAL_GRAPE_RANK_SCORE="${TOTAL_GRAPE_RANK_SCORE::-3}"
 
-cypherResults3=$(sudo cypher-shell -a "$NEO4J_URI" -u "$NEO4J_USER" -p "$NEO4J_PASSWORD" "
+echo "TOTAL_REPORT_COUNT: ${TOTAL_REPORT_COUNT}"
+echo "TOTAL_VERIFIED_REPORT_COUNT: ${TOTAL_VERIFIED_REPORT_COUNT}"
+echo "TOTAL_GRAPE_RANK_SCORE:  ${TOTAL_GRAPE_RANK_SCORE}"
+
+cypherCommand="
 MATCH (u:NostrUser)
 SET u.nip56_totalVerifiedReportCount = $TOTAL_VERIFIED_REPORT_COUNT
 SET u.nip56_totalReportCount = $TOTAL_REPORT_COUNT
 SET u.nip56_totalGrapeRankScore = $TOTAL_GRAPE_RANK_SCORE
-RETURN COUNT(u) AS numReportedUsers")
+RETURN COUNT(u) AS numReportedUsers
+"
+
+echo "cypherCommand: ${cypherCommand}"
+
+cypherResults3=$(sudo cypher-shell -a "$NEO4J_URI" -u "$NEO4J_USER" -p "$NEO4J_PASSWORD" "$cypherCommand")
+
 numReportedUsers="${cypherResults3:11}"
 
 echo "$(date): Finished calculateReportScores"
 echo "$(date): Finished calculateReportScores" >> ${BRAINSTORM_LOG_DIR}/calculateReportScores.log
+
--- src/algos/reports/updateReportTypes.sh
+++ src/algos/reports/updateReportTypes.sh
@@ -13,14 +13,14 @@ CYPHER1="
 MATCH (a:NostrUser)-[r:REPORTS]->(u:NostrUser)
 WHERE r.report_type IS NOT NULL
 AND r.report_type <> ''
-WITH DISTINCT r.report_type AS reportType_g3Rfg5HY
-RETURN reportType_g3Rfg5HY
+WITH DISTINCT r.report_type AS reportType
+RETURN reportType
 "
 
 cypherResults1=$(sudo cypher-shell -a "$NEO4J_URI" -u "$NEO4J_USER" -p "$NEO4J_PASSWORD" "$CYPHER1")
 
-# cypher returns results with 'reportType_g3Rfg5HY' prefix, so we need to remove it
-cypherResults1=$(echo "$cypherResults1" | sed 's/"reportType_g3Rfg5HY": //g')
+# cypher returns results with 'reportType' prefix, so we need to remove the first item in the array
+cypherResults1=(${cypherResults1[@]:1})
 
 # remove existing reportTypes.json
 sudo rm -f ${BRAINSTORM_MODULE_ALGOS_DIR}/reports/reportTypes.json
