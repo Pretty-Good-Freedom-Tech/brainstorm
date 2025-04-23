5b250ef481a13bed6c0ff2f3fad122048aaecda9
David Strayhorn <david.strayhorn4@gmail.com>
2025-04-23 12:11:33 -0400

    organizing nip56 fields in userdata.js endpoint

--- src/api/export/users/queries/userdata.js
+++ src/api/export/users/queries/userdata.js
@@ -132,31 +132,37 @@ function handleGetUserData(req, res) {
             muterCount: user.get('muterCount') ? parseInt(user.get('muterCount').toString()) : 0,
             reportingCount: user.get('reportingCount') ? parseInt(user.get('reportingCount').toString()) : 0,
             reporterCount: user.get('reporterCount') ? parseInt(user.get('reporterCount').toString()) : 0,
-            nip56TotalGrapeRankScore: user.get('nip56TotalGrapeRankScore') ? parseFloat(user.get('nip56TotalGrapeRankScore').toString()) : null,
-            nip56TotalVerifiedReportCount: user.get('nip56TotalVerifiedReportCount') ? parseInt(user.get('nip56TotalVerifiedReportCount').toString()) : 0,
-            nip56TotalReportCount: user.get('nip56TotalReportCount') ? parseInt(user.get('nip56TotalReportCount').toString()) : 0,
-            ...Object.fromEntries(
-              reportTypes.flatMap(reportType => [
-                [
-                  `nip56${reportType}GrapeRankScore`,
-                  user.get(`nip56${reportType}GrapeRankScore`)
-                    ? parseFloat(user.get(`nip56${reportType}GrapeRankScore`).toString())
-                    : null
-                ],
-                [
-                  `nip56${reportType}VerifiedReportCount`,
-                  user.get(`nip56${reportType}VerifiedReportCount`)
-                    ? parseInt(user.get(`nip56${reportType}VerifiedReportCount`).toString())
-                    : 0
-                ],
-                [
-                  `nip56${reportType}ReportCount`,
-                  user.get(`nip56${reportType}ReportCount`)
-                    ? parseInt(user.get(`nip56${reportType}ReportCount`).toString())
-                    : 0
-                ]
-              ])
-            )
+            nip56: {
+              totals: {
+                nip56TotalGrapeRankScore: user.get('nip56TotalGrapeRankScore') ? parseFloat(user.get('nip56TotalGrapeRankScore').toString()) : null,
+                nip56TotalVerifiedReportCount: user.get('nip56TotalVerifiedReportCount') ? parseInt(user.get('nip56TotalVerifiedReportCount').toString()) : 0,
+                nip56TotalReportCount: user.get('nip56TotalReportCount') ? parseInt(user.get('nip56TotalReportCount').toString()) : 0
+              },
+              byReportType: {
+                ...Object.fromEntries(
+                  reportTypes.flatMap(reportType => [
+                    [
+                      `nip56${reportType}GrapeRankScore`,
+                      user.get(`nip56${reportType}GrapeRankScore`)
+                        ? parseFloat(user.get(`nip56${reportType}GrapeRankScore`).toString())
+                        : null
+                    ],
+                    [
+                      `nip56${reportType}VerifiedReportCount`,
+                      user.get(`nip56${reportType}VerifiedReportCount`)
+                        ? parseInt(user.get(`nip56${reportType}VerifiedReportCount`).toString())
+                        : 0
+                    ],
+                    [
+                      `nip56${reportType}ReportCount`,
+                      user.get(`nip56${reportType}ReportCount`)
+                        ? parseInt(user.get(`nip56${reportType}ReportCount`).toString())
+                        : 0
+                    ]
+                  ])
+                )
+              }
+            }
           }
         });
       })
