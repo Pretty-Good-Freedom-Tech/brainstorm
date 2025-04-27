b0e399efdd3bb19004d7eb5d3968d2066813275f
David Strayhorn <david.strayhorn4@gmail.com>
2025-04-27 00:19:49 -0400

    update search profiles endpoint

--- src/api/search/profiles/index.js
+++ src/api/search/profiles/index.js
@@ -16,18 +16,28 @@ const { nip19 } = require('nostr-tools');
  * @param {Object} res - Express response object
  */
 async function handleSearchProfiles(req, res) {
-    const searchField = req.query.searchField; // npub (fragment), pubkey (fragment), kind0 (name, username, about, picture, banner, lnpub)
+    const searchType = req.query.searchType; // npub (fragment), pubkey (fragment), kind0 (name, username, about, picture, banner, lnpub)
     const searchString = req.query.searchString;
     
-    if (!searchField || !searchString) {
+    if (!searchType || !searchString) {
         return res.status(400).json({
             success: false,
-            error: 'Missing search parameter'
+            error: 'Missing search parameter; expecting searchType and searchString'
         });
     }
 
-    // if searchField == npub, then use nip19 to get the pubkey
-    if (searchField === 'npub') {
+    // if searchType is not npub or kind0, return an error
+    if (searchType !== 'npub' && searchType !== 'kind0') {
+        return res.status(400).json({
+            success: false,
+            error: 'Invalid search type'
+        });
+    }
+
+    let searchResult;
+
+    // if searchType == npub, then use nip19 to get the pubkey
+    if (searchType === 'npub') {
         const { pubkey } = nip19.decode(searchString);
         searchResult = pubkey;
         // return here
@@ -38,8 +48,8 @@ async function handleSearchProfiles(req, res) {
         });
     }
 
-    // if searchField == kind0, then use strfry to search
-    if (searchField === 'kind0') {
+    // if searchType == kind0, then use strfry to search
+    if (searchType === 'kind0') {
         // Retrieve all kind 0 events from strfry database
         // execute this bash command:
         // sudo strfry scan '{"kind": [0]}'
@@ -54,7 +64,7 @@ async function handleSearchProfiles(req, res) {
                 return;
             }
             
-            // if searchField == kind0, scan through events to find matches
+            // if searchType == kind0, scan through events to find matches
             const events = JSON.parse(stdout);
             const matches = events.filter(event => event.content.includes(searchString));
             return res.json({
