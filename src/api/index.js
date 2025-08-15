69e380271554436b3c7a9fdb05141499353e1230
David Strayhorn <david.strayhorn4@gmail.com>
2025-08-15 16:58:55 -0400

    cosmetic changes to neo4j health dashboard

--- src/api/index.js
+++ src/api/index.js
@@ -294,8 +294,6 @@ function register(app) {
     // Neo4j Health Dashboard endpoints
     app.get('/api/neo4j-health/complete', neo4jHealth.handleCompleteNeo4jHealth);
     app.get('/api/neo4j-health/alerts', neo4jHealth.handleAlertsNeo4jHealth);
-    app.get('/api/neo4j-health/heap', neo4jHealth.handleHeapNeo4jHealth);
-    app.get('/api/neo4j-health/service', neo4jHealth.handleServiceNeo4jHealth);
 
     console.log('Registered all Brainstorm API endpoints');
 }
