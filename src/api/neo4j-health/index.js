240148e3328101fae108708f20381eba58ad88a2
David Strayhorn <david.strayhorn4@gmail.com>
2025-08-15 16:12:02 -0400

    enabling neo4j-health endpoints

--- src/api/neo4j-health/index.js
+++ src/api/neo4j-health/index.js
@@ -4,13 +4,16 @@
  */
 
 const { handleCompleteNeo4jHealth } = require('./queries/complete-neo4j-health.js');
-// const { handleAlertsNeo4jHealth } = require('./queries/alerts-neo4j-health');
-// const { handleHeapNeo4jHealth } = require('./queries/heap-neo4j-health');
-// const { handleServiceNeo4jHealth } = require('./queries/service-neo4j-health');
+const { handleAlertsNeo4jHealth } = require('./queries/alerts-neo4j-health.js');
+const { handleHeapNeo4jHealth } = require('./queries/heap-neo4j-health.js');
+const { handleServiceNeo4jHealth } = require('./queries/service-neo4j-health.js');
 
 // Export handlers directly - this allows the central router 
 // to register endpoints without creating multiple routers
 module.exports = {
     // Queries (read operations)
-    handleCompleteNeo4jHealth
+    handleCompleteNeo4jHealth,
+    handleAlertsNeo4jHealth,
+    handleHeapNeo4jHealth,
+    handleServiceNeo4jHealth
 };
